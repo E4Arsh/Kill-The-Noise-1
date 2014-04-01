c81be7491acf843e37483f24c11cb72781a64fea
BlakJack <BlakJack@users.noreply.github.com>
2014-04-01 21:41:33 +0530

    Update formats.js

--- config/formats.js
+++ config/formats.js
@@ -106,6 +106,31 @@ exports.Formats = [
 		// no restrictions, for serious (other than team preview)
 		ruleset: ['Team Preview']
 	},
+	{
+		name: "C&E",
+
+                section: "XY Singles",
+
+                ruleset: ['Pokemon', 'Standard Pokebank', 'Team Preview'],
+
+                banlist: ['Soul Dew', 'Uber', 'Farfetchd', 'Kangaskhan', 'Pinsir', 'Heracross', 'Unown', 'Girafarig', 'Dunsparce', 'Shuckle', 'Articuno', 'Zapdos', 'Moltres', 'Qwilfish', 'Corsola', 'Delibird', 'Stantler', 'Smeargle', 'Skarmory', 'Miltank', 'Tauros', 'Lapras', 'Ditto', 'Aerodactyl', 'Sableye', 'Mawile', 'Plusle', 'Minum', 'Volbeat', 'Illumise', 'Torkoal', 'Spinda', 'Seviper', 'Zangoose', 'Lunatone', 'Solrock', 'Castform', 'Kecleon', 'Tropius', 'Absol', 'Relicanth', 'Luvdisc', 'Pachirisu']
+
+        },
+	{
+		name: "DDT",
+		section: "XY Singles",
+
+		ruleset: ['Pokemon', 'Sleep Clause Mod', 'Species Clause', 'Unreleased','OHKO Clause', 'Moody Clause', 'Evasion Moves Clause', 'Endless Battle Clause', 'HP Percentage Mod', 'Team Preview'],
+		banlist: ['OU','Ubers','BrightPowder','Focus Band','Focus Sash','Lax Incense','Quick Claw','Razor Claw','Razor Fang','Chansey','Doublade','Dusclops','Gligar','Jynx','Musharna','Porygon2','Sawk','Scyther','Assist','Attract','Belly Drum + Extreme Speed','Confuse Ray','Flatter','Sticky Web', 'Supersonic','Swagger','Sweet Kiss','Teeter Dance']
+	},
+	{
+		name: "PU",
+		section: "XY Singles",
+
+		ruleset: ['Pokemon', 'Sleep Clause Mod','Unreleased', 'Species Clause', 'OHKO Clause', 'Moody Clause', 'Evasion Moves Clause', 'Endless Battle Clause', 'HP Percentage Mod', 'Team Preview'],
+		banlist: ['OU','NU','UU','Ubers','BrightPowder','Focus Band','Focus Sash','Lax Incense','Quick Claw','Razor Claw','Razor Fang','Chansey','Doublade','Dusclops','Gligar','Jynx','Musharna','Porygon2','Sawk','Scyther','Assist','Attract','Belly Drum + Extreme Speed','Confuse Ray','Flatter','Sticky Web', 'Supersonic','Swagger','Sweet Kiss','Teeter Dance','Shell Smash + Baton Pass','Speed Boost + Baton Pass','Servine + Contrary','Snivy + Contrary','Skrelp + Adaptability']
+	},
+
 
 	// XY Doubles
 	///////////////////////////////////////////////////////////////////
